b7e95a127a380da28b61d15583b3d4ca6fee408c
Brian Staruk <bstaruk@gmail.com>
2016-12-21 16:45:48 -0500

    toggle nav init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,26 @@ import React from 'react';
 import {Link, IndexLink} from 'react-router'
 
 class HeaderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navActive: false
+    };
+  }
+
+  handleToggleNav() {
+    this.setState({
+      navActive: !this.state.navActive
+    });
+  }
+
   render() {
     return (
       <div id="header">
         <h1>brian.staruk.me</h1>
-        <ul role="nav">
+        {this.state.date}
+        <div className="nav-toggle" onClick={this.handleToggleNav.bind(this)}>Toggle Nav</div>
+        <ul id="nav" role="nav" className={this.state.navActive ? 'nav-active' : ''}>
           <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
           <li><Link to="/about" activeClassName="active">About</Link></li>
         </ul>
