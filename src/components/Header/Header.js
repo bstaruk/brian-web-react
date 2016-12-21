107d599af06a76bd9de786e7e8b553a840bbe65c
Brian Staruk <bstaruk@gmail.com>
2016-12-21 16:01:26 -0500

    header & views content buildout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ class HeaderComponent extends React.Component {
   render() {
     return (
       <div id="header">
+        <h1>brian.staruk.me</h1>
         <ul role="nav">
           <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
           <li><Link to="/about" activeClassName="active">About</Link></li>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,10 +10,8 @@ class HomeComponent extends React.Component {
         <Header />
         <Content>
           <h2 className="content-title">Home</h2>
-          <ul>
-            <li>test</li>
-            <li>test</li>
-          </ul>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit massa non porta ultricies. Ut tristique commodo dolor vel ullamcorper. Vestibulum eu facilisis tellus, vel tincidunt nunc. Nullam facilisis justo sem, at imperdiet velit imperdiet pulvinar.</p>
+          <p>Donec ut scelerisque quam. Donec lectus lorem, porta sit amet odio ac, convallis tincidunt dolor. Nunc vel vulputate odio. In diam neque, vulputate eget faucibus hendrerit, finibus sed est. Nulla efficitur ex eu sem hendrerit, nec feugiat arcu ornare. Proin iaculis ut magna quis elementum.</p>
         </Content>
         <Footer />
       </div>
