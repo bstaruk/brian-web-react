212a1a25728461c63f8e24b5993824a82607363f
Brian Staruk <bstaruk@gmail.com>
2016-12-22 08:25:38 -0500

    overlay toggle functionality

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -3,24 +3,15 @@ require('./overlay.scss');
 import React from 'react';
 
 class SkillComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      overlayActive: this.props.active
-    };
-  }
-
   toggleOverlay() {
-    this.setState({
-      overlayActive: !this.state.overlayActive
-    });
+    this.props.onToggle(this.props.slug, false);
   }
 
   render() {
     return (
-      <div className={this.state.overlayActive ? 'overlay overlay--active' : 'overlay'}>
+      <div className={this.props.active ? 'overlay overlay--active' : 'overlay'}>
         <div className="overlay--content">
-          <div className="overlay--toggle" onClick={this.toggleOverlay.bind(this)}>{this.state.overlayActive ? 'Close' : 'Open'} Overlay</div>
+          <div className="overlay--toggle" onClick={this.toggleOverlay.bind(this)}>{this.props.active ? 'Close' : 'Open'} Overlay</div>
           <h3>{this.props.title}</h3>
           {this.props.children}
         </div>
@@ -30,11 +21,13 @@ class SkillComponent extends React.Component {
 }
 
 SkillComponent.propTypes = {
+  slug: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
   active: React.PropTypes.bool.isRequired
 };
 
 SkillComponent.defaultProps = {
+  slug: 'default-overlay',
   title: 'Default Title',
   active: true
 };
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,16 +5,29 @@ import Overlay from '../../Components/Overlay/Overlay';
 import Footer from '../../Components/Footer/Footer';
 
 class HomeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'test1': 'test2'
+    };
+  }
+
+  handleOverlayToggle(overlayId, toggle) {
+    var newState = {};
+    newState[overlayId] = toggle;
+    this.setState(newState);
+  }
+
   render() {
     return (
       <div className="wrapper">
         <Header />
         <Content>
-          <Overlay title="Test Overlay" active={true}>
-            <p>test</p>
+          <Overlay slug="testOverlay1" title="Test Overlay 1" active={this.state.testOverlay1 === true} onToggle={this.handleOverlayToggle.bind(this)}>
+            <p>test one</p>
           </Overlay>
           <h2 className="content-title">Home</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit massa non porta ultricies. Ut tristique commodo dolor vel ullamcorper. Vestibulum eu facilisis tellus, vel tincidunt nunc. Nullam facilisis justo sem, at imperdiet velit imperdiet pulvinar.</p>
+          <p><a onClick={this.handleOverlayToggle.bind(this, 'testOverlay1', true)} href="#">Lorem ipsum dolor</a> sit amet, consectetur adipiscing elit. In hendrerit massa non porta ultricies. Ut tristique commodo dolor vel ullamcorper. Vestibulum eu facilisis tellus, vel tincidunt nunc. Nullam facilisis justo sem, at imperdiet velit imperdiet pulvinar.</p>
           <p>Donec ut scelerisque quam. Donec lectus lorem, porta sit amet odio ac, convallis tincidunt dolor. Nunc vel vulputate odio. In diam neque, vulputate eget faucibus hendrerit, finibus sed est. Nulla efficitur ex eu sem hendrerit, nec feugiat arcu ornare. Proin iaculis ut magna quis elementum.</p>
         </Content>
         <Footer />
