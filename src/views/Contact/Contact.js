b8029966e4b70ffe2c01ae948e21d466c12f9da1
Brian Staruk <bstaruk@gmail.com>
2016-12-24 09:53:59 -0500

    external link class (for icon)

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -7,7 +7,7 @@ class ContactComponent extends React.Component {
       <Content>
         <h2 className="content-title">Contact Me</h2>
         <p>Want to hire me, or just say hello? I'm always around!</p>
-        <p>If your name isn't Mom, chances are I won't pick up your call. I do check my emails more than any person should, though! Shoot me a line at <a href="mailto:brian@staruk.me" target="_blank">brian@staruk.me</a> or connect with me via social media if you'd like to chat.</p>
+        <p>If your name isn't Mom, chances are I won't pick up your call. I do check my emails more than any person should, though! Shoot me a line at <a href="mailto:brian@staruk.me" target="_blank" className="external">brian@staruk.me</a> or connect with me via social media if you'd like to chat.</p>
       </Content>
     );
   }
