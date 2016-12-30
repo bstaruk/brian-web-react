a29f91628ce304e90a74e39e294e3a07c815e57f
Brian Staruk <bstaruk@gmail.com>
2016-12-30 16:42:24 -0500

    lodash basics (rough cut)

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,38 @@
 require('./scss/table.scss');
 
 import React from 'react';
+import _ from 'lodash';
 
 class TableComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    const defaultSortBy = this.props.tableHeaders[0].id;
+    const defaultTableData = _.sortBy(this.props.tableData, defaultSortBy);
+    this.state = {
+      tableData: defaultTableData,
+      sortBy: defaultSortBy,
+      sortDir: 'ASC'
+    };
+    this._handleSort = this._handleSort.bind(this);
+  }
+
+  _handleSort(id) {
+    let newTableData = [];
+    let newSortDir = null;
+    if (this.state.sortBy === id && this.state.sortDir === 'ASC') {
+      newTableData = _.sortBy(this.state.tableData, id).reverse();
+      newSortDir = 'DESC';
+    } else {
+      newTableData = _.sortBy(this.state.tableData, id);
+      newSortDir = 'ASC';
+    }
+    this.setState({
+      sortBy: id,
+      sortDir: newSortDir,
+      tableData: newTableData
+    });
+  }
+
   render() {
     return (
       <table>
@@ -10,11 +40,13 @@ class TableComponent extends React.Component {
         <tr>
           {this.props.tableHeaders.map((cell, index) =>
             <th key={index}>
-              {cell.label}
+              <span onClick={() => this._handleSort(cell.id)} className={this.state.sortBy === cell.id ? 'sortBy' : ''}>
+                {cell.label}
+              </span>
             </th>
           )}
         </tr>
-        {this.props.tableData.map((row, index) =>
+        {this.state.tableData.map((row, index) =>
           <tr key={index}>
             {Object.values(row).map((cell, index) =>
               <td key={index}>
