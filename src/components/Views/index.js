dfca3b070b292917e9448291a47a2573f2e3d0a1
Brian Staruk <bstaruk@gmail.com>
2017-01-03 14:28:34 -0500

    nested experiments & 404 (#22)

--- src/components/Views/index.js
+++ src/components/Views/index.js
@@ -2,10 +2,12 @@ import Home from './Home.js';
 import Contact from './Contact.js';
 import Experiments from './Experiments.js';
 import ExperimentsTable from './Experiments/Table.js';
+import NoMatch from './NoMatch.js';
 
 export {
   Home,
   Contact,
   Experiments,
-  ExperimentsTable
+  ExperimentsTable,
+  NoMatch
 };
