4d18a69fa6e75a3665ae394cea1d72f47fd3838b
Brian Staruk <bstaruk@gmail.com>
2017-01-03 17:28:50 -0500

    TableItem buildout

--- src/components/Views/Experiments/TableItem.js
+++ src/components/Views/Experiments/TableItem.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Content from '../../Content/Content';
 import {Link} from 'react-router';
+import tableData from '../../../assets/json/tableData.json';
 
 class ExperimentsTableItemComponent extends React.Component {
   render() {
     const itemId = this.props.routeParams.item;
-    console.log(itemId);
+    const itemData = tableData.find(function (data) {
+      return data.id == itemId;
+    });
+    console.log(itemData);
     return (
       <Content>
-        <h2 className='content-title'><Link to="/experiments">Experiments</Link> > <Link to="/experiments/table">Table</Link> > Table Item</h2>
-        <p>Table item content.</p>
+        <h2 className='content-title'><Link to="/experiments">Experiments</Link> > <Link
+          to="/experiments/table">Table</Link> > Table Item</h2>
+        {
+          itemData &&
+          <ul>
+            {Object.keys(itemData).map((k, index) =>
+              <li key={index}>
+                {k}: {itemData[k]}
+              </li>
+            )}
+          </ul>
+        }
       </Content>
     );
   }
