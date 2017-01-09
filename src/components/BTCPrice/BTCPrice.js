b5fce1378abd95b43fdaa9bebc7b2e01ff95b762
Brian Staruk <bstaruk@gmail.com>
2017-01-09 08:30:23 -0500

    basic fetch call

--- src/components/BTCPrice/BTCPrice.js
+++ src/components/BTCPrice/BTCPrice.js
@@ -3,10 +3,47 @@ require('./scss/btcprice.scss');
 import React from 'react';
 
 class BTCPriceComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: 0.00,
+      priceError: false
+    };
+    this._getPrice = this._getPrice.bind(this);
+  }
+
+  componentDidMount() {
+    this._getPrice();
+  }
+
+  _getPrice() {
+    fetch('https://api.coinbase.com/v2/prices/BTC-USD/buy').then(r => r.json())
+      .then(
+        data => {
+          this.setState({
+            price: data.data.amount
+          });
+        }
+      )
+      .catch(
+        () => {
+          this.setState({
+            price: 0.00,
+            priceError: true
+          });
+        }
+      );
+  }
+
   render() {
     return (
       <div className="btc-price">
-        <p>BTCPrice.js</p>
+        <p>
+          Current Price: ${this.state.price}
+          {this.state.priceError === true &&
+          <span><br />There was an error fetching the price!</span>
+          }
+        </p>
       </div>
     );
   }
