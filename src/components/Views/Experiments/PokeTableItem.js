fa60ed5b76c9003f659dcd3c6a3d8dfbae053342
Brian Staruk <bstaruk@gmail.com>
2017-01-09 07:02:53 -0500

    item view cleanup

--- src/components/Views/Experiments/PokeTableItem.js
+++ src/components/Views/Experiments/PokeTableItem.js
@@ -11,26 +11,30 @@ class ExpPokeTableItemComponent extends React.Component {
   render() {
     PokeTableStore.init();
     const itemData = PokeTableStore.getRecord('nat', this.props.routeParams.item);
-    const itemTitle = itemData[this.props.route.title];
 
     return (
       <Content>
-        <h2 className='content-title'>
-          <Link to="/experiments">Experiments</Link>
-          <span> > </span>
-          <Link to="/experiments/poketable">Poké Table</Link>
-          <span> > </span>
-          {itemTitle}
-        </h2>
         {
           itemData &&
-          <ul>
-            {Object.keys(itemData).map((k, index) =>
-              <li key={index}>
-                <strong>{PokeTableStore.getSchemaProp(k, 'label')}:</strong> {itemData[k] ? itemData[k] : 'N/A'}
-              </li>
-            )}
-          </ul>
+            <div>
+              <h2 className='content-title'>
+                <Link to="/experiments">Experiments</Link>
+                <span> > </span>
+                <Link to="/experiments/poketable">Poké Table</Link>
+                <span> > </span>
+                {itemData.name} (#{itemData.nat})
+              </h2>
+              <h3>Base Stats</h3>
+              <ul>
+                <li>Health Points: {itemData.hp}</li>
+                <li>Attack: {itemData.atk}</li>
+                <li>Defense: {itemData.def}</li>
+                <li>Special Attack: {itemData.spa}</li>
+                <li>Special Defense: {itemData.spd}</li>
+                <li>Speed: {itemData.spe}</li>
+                <li>Total: {itemData.total}</li>
+              </ul>
+            </div>
         }
       </Content>
     );
