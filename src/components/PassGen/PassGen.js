35891ba7b16adf2d62a1f2a9edb55bf45403357c
Brian Staruk <bstaruk@gmail.com>
2017-01-09 15:07:14 -0500

    component make method init

--- src/components/PassGen/PassGen.js
+++ src/components/PassGen/PassGen.js
@@ -3,10 +3,34 @@ require('./scss/passgen.scss');
 import React from 'react';
 
 class PassGenComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '111111111111111'
+    };
+    this._makeNewPassword = this._makeNewPassword.bind(this);
+  }
+
+  _makeNewPassword() {
+    this.setState({
+      password: '222222222222222'
+    });
+  }
+
   render() {
     return (
       <div className="passgen">
         <p>Hi mom!</p>
+        <form className="passgen--form">
+          <fieldset>
+            <p className="row">
+              <input type="text" name="password" value={this.state.password} readOnly={true} />
+            </p>
+            <p className="row">
+              <button onClick={this._makeNewPassword} type="button">Click</button>
+            </p>
+          </fieldset>
+        </form>
       </div>
     );
   }
