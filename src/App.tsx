760b3378b17aae5a6f986457d34ef9dd1ecbb9e9
Brian Staruk <brian@staruk.net>
2025-05-09 19:37:50 -0400

    starting to build out the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
   return (
     <>
-      <div>
+      <div className="w-full min-h-screen flex flex-col items-center justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="w-10 h-auto" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-10 h-auto" alt="React logo" />
-        </a>
+
+        <h1>Brian Staruk</h1>
       </div>
-      <h1 className="text-red-500">Vite + React</h1>
     </>
   );
 }
