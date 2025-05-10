ada20ed44ed16f6c64165ad136d7e2ca1b8928df
Brian Staruk <brian@staruk.net>
2025-05-09 20:06:35 -0400

    fonts buildout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import viteLogo from '/vite.svg';
-
 function App() {
   return (
     <>
       <div className="w-full min-h-screen flex flex-col items-center justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-10 h-auto" alt="Vite logo" />
-        </a>
-
         <h1>Brian Staruk</h1>
+        <p className="uppercase">Web dude from Boston, MA</p>
       </div>
     </>
   );
