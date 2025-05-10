d5ffe8cd33864256d69a9b3048189b69a19ad9b8
Brian Staruk <brian@staruk.net>
2025-05-09 22:19:09 -0400

    theme & page wrapper buildout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,28 +6,53 @@ import {
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
+const menuItems = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'About',
+    path: '/about',
+  },
+];
+
 // It's the layout component
 export const Route = createRootRoute({
   component: () => (
     <>
       <HeadContent />
 
-      <main className="flex h-screen py-10 gap-8 wrapper-page">
-        <aside className="min-w-40 shrink-0 flex flex-col items-stretch py-4">
-          <nav className="grow flex flex-col gap-2 border-r border-stone-300 pr-8 uppercase">
-            <Link to="/" className="[&.active]:font-bold">
-              Home
-            </Link>{' '}
-            <Link to="/about" className="[&.active]:font-bold">
-              About
-            </Link>
-          </nav>
-        </aside>
+      <div className="h-screen wrapper-page py-5 md:py-8 lg:py-10 flex flex-col gap-5">
+        <header className="text-title uppercase">
+          brian<span className="text-monster-400 px-0.5">.</span>staruk
+          <span className="text-monster-400 pl-0.5">.net</span>
+        </header>
 
-        <section className="grow shrink overflow-y-auto py-4">
-          <Outlet />
-        </section>
-      </main>
+        <main className="grow flex gap-8">
+          <aside className="min-w-40 shrink-0 flex flex-col items-stretch py-4">
+            <nav className="grow flex flex-col gap-2 border-r border-monster-300 pr-8 uppercase">
+              {menuItems.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  className="[&.active]:font-bold"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </nav>
+          </aside>
+
+          <section className="grow shrink overflow-y-auto py-4">
+            <Outlet />
+          </section>
+        </main>
+
+        <footer className="border-t border-monster-400 pt-4">
+          <p>hi mom</p>
+        </footer>
+      </div>
 
       <TanStackRouterDevtools />
     </>
