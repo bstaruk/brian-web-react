4e633b99003e30a03ad231442f9f42fa0c8bc2a4
Brian Staruk <brian@staruk.net>
2025-05-11 15:51:04 -0400

    number buildout

--- src/components/atoms/ScoreboardNumber.tsx
+++ src/components/atoms/ScoreboardNumber.tsx
@@ -9,18 +9,19 @@ const ScoreboardNumber: React.FC<ScoreboardNumberProps> = ({ value }) => {
   const display = String(value).padStart(2, '0'); // Ensure at least 2 digits
 
   return (
-    <div className="bg-monster-950 text-yellow-400 border border-yellow-700 rounded-sm inline-flex text-xl leading-none">
-      {display.split('').map((char, i) => (
-        <span
-          key={i}
-          className={clsx(
-            'relative',
-            'before:content-[""] before:absolute before:inset-0 before:bg-[radial-gradient(circle,_rgba(255,255,255,0.7)_25%,_transparent_26%)] before:bg-size-[0.25rem_0.25rem] before:opacity-80 p-1 text-[1.5rem] font-medium',
-          )}
-        >
-          <span className="relative z-10">{char}</span>
-        </span>
-      ))}
+    <div className="bg-monster-950 text-yellow-400 border border-yellow-700 rounded-sm inline-flex leading-none">
+      <div
+        className={clsx(
+          'relative p-2',
+          'before:content-[""] before:absolute before:inset-0 before:bg-[radial-gradient(circle,_rgba(255,255,255,0.7)_25%,_transparent_26%)] before:bg-size-[0.25rem_0.25rem] before:opacity-80 p-1 text-[1.5rem] flex gap-1.5',
+        )}
+      >
+        {display.split('').map((char, i) => (
+          <span key={i} className="relative z-10">
+            {char}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
