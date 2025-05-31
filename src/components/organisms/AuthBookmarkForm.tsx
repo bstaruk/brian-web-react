88535ea3db3b145fe85ff5d81f365ec189aea71c
Brian Staruk <brian@staruk.net>
2025-05-31 17:44:58 -0400

    font weight cleanup

--- src/components/organisms/AuthBookmarkForm.tsx
+++ src/components/organisms/AuthBookmarkForm.tsx
@@ -62,13 +62,13 @@ function AuthBookmarkForm() {
       className="flex flex-col items-start gap-4"
     >
       <div className="flex flex-col gap-2">
-        <h5 className="font-medium">Your bookmark is...</h5>
+        <h5>Your bookmark is...</h5>
         <p>
           <Link
             href={bookmark}
             target="_blank"
             rel="noopener noreferrer"
-            className="font-mono text-mono font-bold"
+            className="font-bold"
           >
             {bookmark}
           </Link>
