23d7daf15a02669e2ee08c13dea365c65a6f8a72
Brian Staruk <brian@staruk.net>
2025-05-31 12:37:49 -0400

    link variants init

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import { createLink, type LinkComponent } from '@tanstack/react-router';
+import clsx from 'clsx';
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: string;
+  variant?: 'marathon' | 'marathon-light';
 }
 
 const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ children, ...rest }, ref) => {
+  ({ children, variant = 'marathon', ...rest }, ref) => {
     return (
       <a
         {...rest}
         {...{ ref }}
-        className="text-marathon-400 font-medium underline hover:no-underline"
+        className={clsx('font-medium underline hover:no-underline', {
+          'text-marathon-400 hover:text-marathon-100': variant === 'marathon',
+          'text-marathon-200 hover:text-marathon-100':
+            variant === 'marathon-light',
+        })}
       >
         {children}
       </a>
