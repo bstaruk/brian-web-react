a9632e3ea20f8608180af94bf8052dd850d45b46
Brian Staruk <brian@staruk.net>
2025-05-31 16:31:05 -0400

    goodbye relative imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import Link from '../components/atoms/Link';
+import Link from 'atoms/Link';
 
 export const Route = createFileRoute('/')({
   component: Home,
