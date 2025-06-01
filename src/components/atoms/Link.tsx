b99072f53ffdc3c775dc1263c97f0eb6494f5875
Brian Staruk <brian@staruk.net>
2025-06-01 15:06:03 -0400

    add missing svg fill

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -21,7 +21,7 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
               variant === 'marathon',
             'text-marathon-200 hover:text-marathon-100 focus:text-marathon-100 [svg&]fill-marathon-200 hover:[svg&]fill-marathon-100 focus:[svg&]fill-marathon-100':
               variant === 'marathon-light',
-            'text-stone-100': variant === 'white',
+            'text-stone-100 [svg&]fill-stone-100': variant === 'white',
             'text-monster-100 hover:text-monster-50 focus:text-monster-50 [svg&]fill-monster-100 hover:[svg&]fill-monster-50 focus:[svg&]fill-monster-50':
               variant === 'monster',
           },
