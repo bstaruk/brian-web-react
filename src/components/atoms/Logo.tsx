a53b4482a3d83387222e3afadcf3d6681649a11a
Brian Staruk <brian@staruk.net>
2025-06-01 16:17:21 -0400

    focus state tweaks

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className }) => (
   <Link
     to="/"
     className={clsx(
-      'size-11 rounded-full border-4 border-monster-300 flex items-center justify-center shadow-xs hover:border-monster-200',
+      'size-11 rounded-full border-4 border-monster-300 flex items-center justify-center shadow-xs hover:border-monster-200 focus:border-monster-200 active:border-monster-200 outline-0',
       className,
     )}
   >
