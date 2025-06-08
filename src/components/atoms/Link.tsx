fff764aa1d39736d5552764ab6d6a69333ee8781
Brian Staruk <brian@staruk.net>
2025-06-08 12:02:16 -0400

    LinkButton (buttons that look like links) init

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -2,31 +2,38 @@ import * as React from 'react';
 import { createLink, type LinkComponent } from '@tanstack/react-router';
 import clsx from 'clsx';
 
-interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  children: React.ReactNode;
+interface BaseButtonProps {
   variant?: 'marathon' | 'marathon-light' | 'white' | 'monster';
 }
+
+type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  BaseButtonProps;
+type LinkButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  BaseButtonProps;
+
+const linkClassNames = (
+  variant: BaseButtonProps['variant'],
+  className?: string,
+) => {
+  return clsx(
+    className,
+    'font-medium hover:underline focus:underline outline-0',
+    {
+      'text-marathon-400 hover:text-marathon-100 focus:text-marathon-100 [svg&]fill-marathon-400 hover:[svg&]fill-marathon-100 focus:[svg&]fill-marathon-100':
+        variant === 'marathon',
+      'text-marathon-200 hover:text-marathon-100 focus:text-marathon-100 [svg&]fill-marathon-200 hover:[svg&]fill-marathon-100 focus:[svg&]fill-marathon-100':
+        variant === 'marathon-light',
+      'text-stone-100 [svg&]fill-stone-100': variant === 'white',
+      'text-monster-100 hover:text-monster-50 focus:text-monster-50 [svg&]fill-monster-100 hover:[svg&]fill-monster-50 focus:[svg&]fill-monster-50':
+        variant === 'monster',
+    },
+  );
+};
 
 const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
   ({ children, className, variant = 'marathon', ...rest }, ref) => {
     return (
-      <a
-        {...rest}
-        {...{ ref }}
-        className={clsx(
-          className,
-          'font-medium hover:underline focus:underline outline-0',
-          {
-            'text-marathon-400 hover:text-marathon-100 focus:text-marathon-100 [svg&]fill-marathon-400 hover:[svg&]fill-marathon-100 focus:[svg&]fill-marathon-100':
-              variant === 'marathon',
-            'text-marathon-200 hover:text-marathon-100 focus:text-marathon-100 [svg&]fill-marathon-200 hover:[svg&]fill-marathon-100 focus:[svg&]fill-marathon-100':
-              variant === 'marathon-light',
-            'text-stone-100 [svg&]fill-stone-100': variant === 'white',
-            'text-monster-100 hover:text-monster-50 focus:text-monster-50 [svg&]fill-monster-100 hover:[svg&]fill-monster-50 focus:[svg&]fill-monster-50':
-              variant === 'monster',
-          },
-        )}
-      >
+      <a {...rest} {...{ ref }} className={linkClassNames(variant, className)}>
         {children}
       </a>
     );
@@ -44,5 +51,19 @@ export const RouterLink: LinkComponent<typeof Link> = (props) => {
     />
   );
 };
+
+export const LinkButton = React.forwardRef<HTMLButtonElement, LinkButtonProps>(
+  ({ children, className, variant = 'marathon', ...rest }, ref) => {
+    return (
+      <button
+        {...rest}
+        {...{ ref }}
+        className={linkClassNames(variant, className)}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 export default Link;
