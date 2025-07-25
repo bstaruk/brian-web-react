75444fd5948fa418a1a7fc73cb8ece75b94c542b
Brian Staruk <brian@staruk.net>
2025-07-25 18:08:00 -0400

    logo structure improvements
    should align more reliably cross-device

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -9,11 +9,13 @@ const Logo: React.FC<LogoProps> = ({ className }) => (
   <Link
     to="/"
     className={clsx(
-      'size-11 rounded-full border-4 border-monster-300 flex items-center justify-center shadow-xs hover:border-monster-200 focus:border-monster-200 active:border-monster-200 outline-0',
+      'relative size-12 rounded-full border-5 border-monster-300 shadow-xs hover:border-monster-200 focus:border-monster-200 active:border-monster-200 outline-0',
       className,
     )}
   >
-    <div className="text-h2 font-bold translate-x-0.25">B</div>
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-h1 font-bold">
+      B
+    </div>
   </Link>
 );
 
