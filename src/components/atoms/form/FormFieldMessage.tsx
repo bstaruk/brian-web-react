8d45083de1b28647754a055fc919e028db16ca24
Brian Staruk <brian@staruk.net>
2025-09-01 15:53:21 -0400

    validation message theme tweaks

--- src/components/atoms/form/FormFieldMessage.tsx
+++ src/components/atoms/form/FormFieldMessage.tsx
@@ -8,7 +8,7 @@ const FormFieldMessage: React.FC<FormFieldMessageProps> = ({ field }) => {
   if (!field.state.meta.isTouched || !!field.state.meta.isValid) return null;
 
   return (
-    <p className="text-sm text-red-200">
+    <p className="font-medium text-sm text-marathon-400">
       {field.state.meta.errors
         .map((error: { message: string }) => error.message)
         .join(', ')}
